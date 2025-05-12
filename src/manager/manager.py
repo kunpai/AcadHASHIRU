ab59793e162fe173f2ba3763eb15a1d9a2294959
Kunal Pai <kunpai@ucdavis.edu>
2025-05-12 08:15:22 -0700

    MPS support for memories for Mac users

--- src/manager/manager.py
+++ src/manager/manager.py
@@ -164,7 +164,12 @@ class GeminiManager:
             return []
         top_k = min(k, len(memories))
         # Semantic Retrieval with GPU
-        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        if torch.cuda.is_available():
+            device = 'cuda'
+        elif torch.backends.mps.is_available() and torch.backends.mps.is_built():
+            device = 'mps'
+        else:
+            device = 'cpu'
         print(f"Using device: {device}")
         model = SentenceTransformer('all-MiniLM-L6-v2', device=device)
         doc_embeddings = model.encode(memories, convert_to_tensor=True, device=device)
