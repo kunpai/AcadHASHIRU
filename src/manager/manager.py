3fa3bab553577572dc2d07f3feb483340df2d64d
Parth Shah <helloparthshah@gmail.com>
2025-05-26 20:16:49 -0700

    Updated prompt

--- src/manager/manager.py
+++ src/manager/manager.py
@@ -44,7 +44,7 @@ def format_tool_response(response, indent=2):
 
 
 class GeminiManager:
-    def __init__(self, system_prompt_file="./src/models/system5.prompt",
+    def __init__(self, system_prompt_file="./src/models/system6.prompt",
                  gemini_model="gemini-2.5-pro-exp-03-25",
                  modes: List[Mode] = []):
         load_dotenv()
